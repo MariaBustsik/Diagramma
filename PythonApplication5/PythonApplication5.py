56cf6e74e849ffdbe3d2ec4a6cb43af89eaf9c64
Maria Bustsik <maria.bustsik@outlook.com>
2021-01-08 19:06:22 +0200

    1 ylesanne

--- PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5.py
@@ -2,3 +2,16 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import colorama
+
+fail=open("data.txt","r")
+mas1=[]
+mas2=[]
+for line in fail:
+    n=line.find(",")
+    mas1.append(line[0:n].strip())
+    mas2.append(float(line[n+1:len(line)].strip()))
+fail.close()
+plt.title("IKT turvameetodite kasutamise osatähtsus ettevõttes: 2018")
+plt.barh(mas1,mas2,color="pink")
+plt.show()

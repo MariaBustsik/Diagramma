2517571bfa1dd364e23ba4cd0447e90145868d43
Maria Bustsik <maria.bustsik@outlook.com>
2021-01-08 19:11:46 +0200

    .

--- PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5.py
@@ -7,11 +7,13 @@ import colorama
 fail=open("data.txt","r")
 mas1=[]
 mas2=[]
+
 for line in fail:
     n=line.find(",")
     mas1.append(line[0:n].strip())
     mas2.append(float(line[n+1:len(line)].strip()))
 fail.close()
+
 plt.title("IKT turvameetodite kasutamise osatähtsus ettevõttes: 2018")
 plt.barh(mas1,mas2,color="pink")
 plt.show()
